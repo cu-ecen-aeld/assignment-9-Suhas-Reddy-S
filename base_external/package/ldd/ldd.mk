ac8d4c69856f40105d44d3628c0e8279d14cb17f
suhas <suhas@Ubuntu20.04.4.myguest.virtualbox.org>
2024-03-11 02:25:28 -0600

    resolved permission issues

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,7 +1,7 @@
 
 ##############################################################
 #
-# ldd
+# LDD
 #
 ##############################################################
 
@@ -10,14 +10,6 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-Suhas-Reddy-S.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules scull
-
-define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-
-endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
