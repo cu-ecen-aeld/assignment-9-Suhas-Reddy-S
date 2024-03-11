cf5a4278e3dd4306c7c1094c1e3f516f258a8a59
suhas <suhas@Ubuntu20.04.4.myguest.virtualbox.org>
2024-03-10 23:30:43 -0600

    split subdir

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,7 +9,8 @@ LDD_VERSION = 96c5c9b1f10b2d8209a43bd1a8671fc732663ab2
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-Suhas-Reddy-S.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_SUBDIRS = misc-modules 
+LDD_MODULE_SUBDIRS += scull
 
 define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
