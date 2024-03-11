2eef6e3985f8ac081af0f4b5f102dc3c9f66501b
suhas <suhas@Ubuntu20.04.4.myguest.virtualbox.org>
2024-03-10 23:54:22 -0600

    trial

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,14 +9,13 @@ LDD_VERSION = 96c5c9b1f10b2d8209a43bd1a8671fc732663ab2
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-Suhas-Reddy-S.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = misc-modules 
-LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_SUBDIRS = misc-modules scull
 
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 
 endef
 
